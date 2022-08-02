763d4308762ca002189a53f4d5c2796bf9878e97
HeeSeop Kim <khs37705@naver.com>
2022-08-02 18:34:45 +0900

    아이템 프리팹들 전부 바닥에 있으면 플레이어와 충돌하지 않게 스크립트 수정

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -7,11 +7,28 @@ public class Item : MonoBehaviour
     //열거형(아이템 타입지정) 
     public enum Type { Ammo, Coin, Grenade, Heart, Weapon };  //타입(총알, 동전, 수류탄, 하트, 무기)
     public Type type; //타입을 엔진에서 지정
-
     public int value; //아이템 번호 혹은 아이템 수량
+    new Rigidbody rigidbody;
+    new SphereCollider collider;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        collider = GetComponent<SphereCollider>();
+
+    }
 
     private void Update()
     {
         transform.Rotate(Vector3.up * 25 * Time.deltaTime); //회전
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            rigidbody.isKinematic = true; 
+            collider.enabled = false;
+        }
     }
 }

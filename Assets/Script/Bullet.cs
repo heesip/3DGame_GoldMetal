64d5ef7b86d3cf2532bce258d7cbd834cbff9dba
HeeSeop Kim <khs37705@naver.com>
2022-08-03 15:29:16 +0900

    몬스터 피격처리 생성

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -12,10 +12,13 @@ public class Bullet : MonoBehaviour
         if (collision.gameObject.tag == "Floor") //바닥에 닿으면 (탄피에 사용)
         {
             Destroy(gameObject, 2.5f); //자신을 2.5초 후 파괴
+
         }
-        else if (collision.gameObject.tag == "Wall") //벽에 닿으면 (총알에 사용)
-        {
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Wall") //벽에 닿으면 (총알에 사용)
             Destroy(gameObject); //자신을 즉시 파괴
-        }
+
     }
 }
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth; //최대 체력
+    public int curHealth; //현재 체력
+
+
+    Rigidbody enemyrigid;
+    BoxCollider boxcol;
+    Material mat;
+
+    private void Awake()
+    {
+        enemyrigid = GetComponent<Rigidbody>();
+        boxcol = GetComponent<BoxCollider>();
+        mat = GetComponent<MeshRenderer>().material;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Melee") //근접무기가 닿으면
+        {
+            Weapon weapon = other.GetComponent<Weapon>();
+            curHealth -= weapon.damage; //현재 체력을 무기 데미지 만큼 감소 
+            //Enemy 위치 값에서 enemy를 때린 무기 위치값을 빼서 값을 reactVec에 저장
+            Vector3 reactVec = transform.position - other.transform.position; 
+            StartCoroutine(OnDamage(reactVec));
+        }
+        else if(other.tag == "Bullet") //총알에 닿으면
+        {
+            Bullet bullet = other.GetComponent<Bullet>();
+            curHealth -= bullet.damage; //현재 체력을 총알 데미지 만큼 감소
+            //Enemy 위치 값에서 enemy를 때린 무기 위치값을 빼서 값을 reactVec에 저장
+            Vector3 reactVec = transform.position - other.transform.position;
+            Destroy(other.gameObject);
+            StartCoroutine(OnDamage(reactVec));
+        }
+    }
+
+    IEnumerator OnDamage(Vector3 reactVec) //피격 함수 (피격시 받은 reactVec값 받아옴)
+    {
+        mat.color = Color.red; //빨간색으로 변함
+        yield return new WaitForSeconds(0.1f);
+
+        if(curHealth > 0) //현재 체력이 0보다 크면
+        {
+            mat.color = Color.white; //원래 흰색으로 변경
+        }
+
+        else //0보다 작거나 같으면
+        {
+            mat.color = Color.gray;  //회색으로 변경
+            gameObject.layer = 11; //레이어를 11번(EnemyDead)로 변경
+
+            reactVec = reactVec.normalized; //react값이 어느방향이든 같은 값을 가져옴
+            reactVec += Vector3.up; //react값을 위쪽 방향으로 더해줌
+            enemyrigid.AddForce(reactVec * 5, ForceMode.Impulse); //그 값을 5만큼 곱해서 힘을 가함
+
+            Destroy(gameObject,1.2f); //1.2초 후 파괴
+        }
+    }
+
+}

8e6782f701b24eb4d58ce4bb71f9f28f5573c9cd
HeeSeop Kim <khs37705@naver.com>
2022-08-03 19:07:56 +0900

    수류탄 구현 완성 1. Enemy 스크립트 수정 2. Grenade 스크립트 수정

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -7,7 +7,6 @@ public class Enemy : MonoBehaviour
     public int maxHealth; //최대 체력
     public int curHealth; //현재 체력
 
-
     Rigidbody enemyrigid;
     BoxCollider boxcol;
     Material mat;
@@ -28,7 +27,7 @@ public class Enemy : MonoBehaviour
             curHealth -= weapon.damage; //현재 체력을 무기 데미지 만큼 감소 
             //Enemy 위치 값에서 enemy를 때린 무기 위치값을 빼서 값을 reactVec에 저장
             Vector3 reactVec = transform.position - other.transform.position; 
-            StartCoroutine(OnDamage(reactVec));
+            StartCoroutine(OnDamage(reactVec,false));
         }
         else if(other.tag == "Bullet") //총알에 닿으면
         {
@@ -37,11 +36,11 @@ public class Enemy : MonoBehaviour
             //Enemy 위치 값에서 enemy를 때린 무기 위치값을 빼서 값을 reactVec에 저장
             Vector3 reactVec = transform.position - other.transform.position;
             Destroy(other.gameObject);
-            StartCoroutine(OnDamage(reactVec));
+            StartCoroutine(OnDamage(reactVec,false));
         }
     }
 
-    IEnumerator OnDamage(Vector3 reactVec) //피격 함수 (피격시 받은 reactVec값 받아옴)
+    IEnumerator OnDamage(Vector3 reactVec, bool isGrenade) //피격 함수 (피격시 받은 reactVec값 받아옴), 피격 준 물체가 수류탄인지 확인
     {
         mat.color = Color.red; //빨간색으로 변함
         yield return new WaitForSeconds(0.1f);
@@ -56,12 +55,32 @@ public class Enemy : MonoBehaviour
             mat.color = Color.gray;  //회색으로 변경
             gameObject.layer = 11; //레이어를 11번(EnemyDead)로 변경
 
-            reactVec = reactVec.normalized; //react값이 어느방향이든 같은 값을 가져옴
-            reactVec += Vector3.up; //react값을 위쪽 방향으로 더해줌
-            enemyrigid.AddForce(reactVec * 5, ForceMode.Impulse); //그 값을 5만큼 곱해서 힘을 가함
+            if (isGrenade) //데미지를 준게 수류탄이면
+            {
+                reactVec = reactVec.normalized; //react값이 어느방향이든 같은 값을 가져옴
+                reactVec += Vector3.up * 3.5f; //react값을 위쪽 방향으로 더해줌
+                enemyrigid.freezeRotation = false; //회전이 가능하게 변경
+                enemyrigid.AddForce(reactVec * 5, ForceMode.Impulse); //그 값을 5만큼 곱해서 힘을 가함
+                enemyrigid.AddTorque(reactVec * 15, ForceMode.Impulse); //그 값을 15만큼 곱해서 회전을 가함
 
+            }
+            else //그게아니면
+            {
+                reactVec = reactVec.normalized; //react값이 어느방향이든 같은 값을 가져옴
+                reactVec += Vector3.up; //react값을 위쪽 방향으로 더해줌
+                enemyrigid.AddForce(reactVec * 5, ForceMode.Impulse); //그 값을 5만큼 곱해서 힘을 가함
+            }
+            
+            
+           
             Destroy(gameObject,1.2f); //1.2초 후 파괴
         }
     }
+    public void HitByGrenade(Vector3 explosionPos) //수류탄 피격함수
+    {
+        curHealth -= 100; //체력 100감소
+        Vector3 reactVec = transform.position - explosionPos; 
+        StartCoroutine(OnDamage(reactVec,true));
 
+    }
 }
--- Assets/Script/Grenade.cs
+++ Assets/Script/Grenade.cs
@@ -28,6 +28,16 @@ public class Grenade : MonoBehaviour
         meshObj.SetActive(false); //수류탄 이미지 비활성화
         effectObj.SetActive(true); //폭발 이미지 활성화
 
+        //레이캐스트 배열을 만들어 보이지는 않지만 수류탄 주변으로 반지름 15크기 안에 들어오는 적들의 정보를 가져옴
+        RaycastHit[] rayHits = Physics.SphereCastAll(transform.position, 15,
+                                                     Vector3.up, 0f,
+                                                     LayerMask.GetMask("Enemy"));
+
+        foreach(RaycastHit hitObj in rayHits) //가져온 적들에게 전부 수류탄 데미지 + 위치 정보를 적용
+        {
+            hitObj.transform.GetComponent<Enemy>().HitByGrenade(transform.position);
+        }
+
         Destroy(gameObject, 1.5f); //1.5초뒤 자신을 파괴
     }
 }

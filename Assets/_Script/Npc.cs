35d1972167f65def41f2ed96027b3fb39ba9db34
HeeSeop Kim <khs37705@naver.com>
2022-08-10 19:01:32 +0900

    NPC 틀 잡음

---
+++ Assets/_Script/Npc.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Npc : MonoBehaviour
+{
+    Animator anim;
+    private void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            anim.SetTrigger("doHello");
+        }
+    }
+}
